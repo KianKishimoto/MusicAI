d214873720a98b38e579b3e1dcfa98918132c502
KianKishimoto <112417570+KianKishimoto@users.noreply.github.com>
2022-09-14 15:19:04 -0400

    developed youtube video download function

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-def test(int, asdf):
-    print(int)
-    print(asdf)
-    print("test12")
+from pytube import YouTube
+
+def fetchVid(link):
+    
+    try:
+        vid = YouTube(link)
+        vid.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download("media")
+    except:
+        print("Connection failed. Try again later.")
+    print("Title: ", vid.title)
+    print("Length: ", vid.length)
+        
+    
 
 
 
 if __name__ == "__main__":
-    test(1, "asdf")
+    print("Paste music video link below: ")
+    fetchVid(input())
